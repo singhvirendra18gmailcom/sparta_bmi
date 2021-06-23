e1f5e28af87560a58f8d684f0a4fc86f5f6d84ba
singh <singh@DESKTOP-5NBQE9U>
2021-06-23 08:40:07 +0530

    create new spring boot project for crud operations BMI ( body mass index)

---
+++ parent/sparta-bmi-core/src/main/java/com/bmi/core/SpartaBmiCoreApplication.java
@@ -0,0 +1,13 @@
+package com.bmi.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaBmiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaBmiCoreApplication.class, args);
+	}
+
+}
---
+++ parent/sparta-bmi-core/src/test/java/com/bmi/core/SpartaBmiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmi.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaBmiCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

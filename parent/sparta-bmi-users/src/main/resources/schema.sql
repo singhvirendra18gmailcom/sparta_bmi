f245b3aee0dca78ca6fd3a2ee22f47e0398006d9
singh <singh@DESKTOP-5NBQE9U>
2021-06-23 15:41:33 +0530

    db initializations script

---
+++ parent/sparta-bmi-users/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE  `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `weight` varchar(45) DEFAULT NULL,
+  `height` varchar(45) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='users table contains information for sparta bmi users information.';
